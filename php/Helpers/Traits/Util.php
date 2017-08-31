70d980f268a0c13ff79e8174e486f039b020c5f4
Subak Systems <info@subak.jp>
2017-09-01 00:21:44 +0900

    tmp_dir,content_dirを外出し。execメソッドをproc_openで書き直し

---
+++ php/Helpers/Traits/Util.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Helpers\Traits;
+
+trait Util {
+  public function exec(string $cmd, string $stdin="") : string {
+
+    $descriptorspec = [
+      1 => ['pipe', 'w'],
+      2 => ['pipe', 'w']
+    ];
+
+    if($stdin){
+      $descriptorspec[0] = ['pipe', 'r'];
+    }
+    
+    $process = proc_open($cmd, $descriptorspec, $pipes);
+
+    $stdout = "";
+    $stderr = "";
+    
+    if(!is_resource($process)) {
+      throw new \Exception($cmd);
+    }
+    
+    if($stdin) {
+      fwrite($pipes[0], $stdin);
+      fclose($pipes[0]);
+    }
+
+    $stdout = stream_get_contents($pipes[1]);
+    $stderr = stream_get_contents($pipes[2]);
+      
+    fclose($pipes[1]);
+    fclose($pipes[2]);
+
+    proc_close($process);
+
+    return $stdout;
+  }
+}
